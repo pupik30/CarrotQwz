9f6e5f53ec99693ed4088d4105e6758394cfa6c5
Роман Марковин <MarkovinR@kbkit.pri>
2026-06-06 11:51:34 +0200

    Two

---
+++ Front/CarrotQwz/Photos.js
@@ -0,0 +1,17 @@
+import logoTDA from './src/assets/logoTDA.png'
+import search from './src/assets/search.png'
+import Arrow from './src/assets/Arrow.png'
+import navigation from './src/assets/navigation.png'
+import phone from './src/assets/phone.png'
+import Mail from './src/assets/Mail.png'
+
+
+
+export const Photos = {
+    logoTDA:logoTDA,
+    search:search,
+    Arrow:Arrow,
+    navigation:navigation,
+    phone:phone,
+    Mail:Mail,
+}
--- Front/CarrotQwz/src/App.jsx
+++ Front/CarrotQwz/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React, { StrictMode,} from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Common/Header/Header.jsx'
+import Bottom from './Common/Bottom/Bottom.jsx'
+import MainPage from './MainPage/MainPage.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <StrictMode>
-        <Header />
-          
-      </StrictMode>,
+        <BrowserRouter>
+          <Header/>
+            <Routes>
+                <Route path='/' index element = {<MainPage/>}/>
+                {/* <Route path='MainPage' element={<MainPage/>}/> */}
+            </Routes>  
+          <Bottom />
+        </BrowserRouter>
+    </StrictMode>
     </>
   )
 }
